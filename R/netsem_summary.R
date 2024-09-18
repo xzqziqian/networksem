1af87760ef4b091b595b2c2a704a7953a55d4596
Ziqian Xu <38840988+iasnobmatsu@users.noreply.github.com>
2024-09-18 15:24:53 -0400

    fix summary

--- R/netsem_summary.R
+++ R/netsem_summary.R
@@ -3,6 +3,7 @@
 #' @return a summary of the networksem output
 #' @export
 summary.networksem <- function(res){
+
   otype = "obs"
   if (class(res$estimates)=="list"){
     otype = "lsm"
@@ -14,65 +15,22 @@ summary.networksem <- function(res){
   cat(res$model)
   cat("\n\n")
 
-  # cat ("summary from using network data in SEM:\n")
 
-  if (otype=="obs"){
-    sem.out <- lavaan::parameterestimates(res$estimates)
-    cat(sem.out[1,1])
-    cat("SEM estimates with network variables: \n")
-    for (col in colnames(sem.out)){
-      cat(col)
-      cat("\t")
-    }
-    for (i in 1:nrow(sem.out)){
-      for (j in 1:ncol(sem.out)){
-        cat(sem.out[i,j])
-        cat("\t")
-      }
-      cat(sem.out[i,1])
-      cat("\n")
-    }
+  if (otype == "obs"){
+    lvsummary <- getMethod("summary",signature(object="lavaan"))
+    cat("The SEM output:\n")
+    print(lvsummary(res$estimates, fit = T))
   }else{
-    sem.out <- lavaan::parameterestimates(res$estimates$sem.es)
-    cat(sem.out[1,1])
-    cat("SEM estimates with network variables: \n")
-    for (col in colnames(sem.out)){
-      cat(col)
-      cat("\t")
+    lvsummary <- getMethod("summary",signature(object="lavaan"))
+    cat("The SEM output:\n")
+    print(lvsummary(res$estimates$sem.es, fit = T))
+
+    lvsummary <- getMethod("summary",signature(object="lavaan"))
+    cat("The LSM output:\n")
+    for (lsmout in res$estimates$lsm.es){
+      print(summary(lsmout))
     }
-    for (i in 1:nrow(sem.out)){
-      for (j in 1:ncol(sem.out)){
-        cat(sem.out[i,j])
-        cat("\t")
-      }
-      cat(sem.out[i,1])
-      cat("\n")
-    }
-
-
-
-
   }
-
-
-
 }
 
 
-# conv <- function(res){
-#   otype = "obs"
-#   if (class(res$estimates)=="list"){
-#     otype = "lsm"
-#   }
-#
-#   out = list()
-#   if (otype=="obs"){
-#     out[[1]] = capture.output(summary(res$estimates))
-#   } else {
-#     out[[1]] = (summary(res$estimates$sem.es))
-#     for (i in 1:length(res$estimates$lsm.es)){
-#       out[[i+1]] = capture.output(summary(res$estimates$lsm.es[[i]]))
-#     }
-#   }
-#   return(out)
-# }
