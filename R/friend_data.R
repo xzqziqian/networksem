a7f2a287ca78d9a834328de6e89d74ee3d6af8dd
Ziqian Xu <38840988+xzqziqian@users.noreply.github.com>
2025-05-09 21:02:02 +0800

    typo fix

--- R/friend_data.R
+++ R/friend_data.R
@@ -1,11 +1,10 @@
 #' Friendship network data
 #'
-#' A dataset with friendship network data of 165 undergaduate students in China, and also contains information extroversion and depression scores
+#' A dataset with friendship network data of 165 undergraduate students in China, and also contains information extroversion and depression scores
 #'
 #' @format A list with two elements
-#' A data frame with 7,240 rows and 60 columns:
 #' \describe{
-#'   \item{network}{A 165 x 165 adjacency matrix indicating friendship ties.}
+#'   \item{network}{A list containing a 165 x 165 adjacency matrix indicating friendship ties.}
 #'   \item{non_network}{A dataframe with 165 rows and 11 columns containing variables on depression from the
 #'   Patient Health Questionnaire and extroversion from the mini-IPIP scale.}
 #' }
@@ -14,8 +13,8 @@
 #'
 #' @examples
 #' data("friend_data")
-#' dim(friend_data$network)
-#' head(friend_data$non_network)
+#' length(friend_data$network)
+#' head(friend_data$nonnetwork)
 #'
 #' @source <https://bigsem.org/>
 "friend_data"
